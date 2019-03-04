45a5c27be79c4cf8454f2371e3e7eb2fb9fa1872
Andrey Strogush <magistrogush@gmail.com>
2019-03-04 12:29:50 +0200

    myArray + interface

---
+++ src/com/homework/Main.java
@@ -0,0 +1,20 @@
+package com.homework;
+
+public class Main {
+    public static void main(String[] args) {
+      MyArrayList<Integer> test = new MyArrayList<>();
+//      test.add(4);
+//      test.add(6);
+//      test.add(99);
+//      test.add(33);
+//        System.out.println(test.get(1));
+//        test.set(1,66);
+//        System.out.println(test.get(1));
+    }
+}
+
+/*
+2.1 Реализовать ArrayList Написать свой класс com.homework.MyArrayList как аналог классу ArrayList.
+ Можно использовать 1 массив для хранения данных.
+Реализовать следующие методы: add, remove, clear, size, get;
+ */
---
+++ src/com/homework/MyArrayList.java
@@ -0,0 +1,31 @@
+package com.homework;
+
+public class MyArrayList<E> implements MyList<E> {
+    private int size;
+
+
+    @Override
+    public void add(E e) {
+        
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+}
